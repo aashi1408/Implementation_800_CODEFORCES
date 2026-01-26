e9e55a0c1ec53ff5555b4a602eba3be83979a5d2
aashi1408 <aashiv2525@gmail.com>
2026-01-26 12:05:36 +0530

    (9)

--- TRAM.cpp
+++ TRAM.cpp
@@ -10,17 +10,18 @@ int main()
 	int t;
 	cin>>t;
 	
-	int mx_capacity = 0 , depart, arrive;
+	int mn_capacity = 0 , depart, arrive;
 	
 	vector<int> arr(t);
 	
 	for(int i=0 ; i<t ; i++)
 	{
 		cin>>depart>>arrive;
-		mx_capacity-=depart;
-		mx_capacity+=arrive;
-		arr[i] = mx_capacity;
+		mn_capacity-=depart;
+		mn_capacity+=arrive;
+		arr[i] = mn_capacity;
 	}
 	cout<<*max_element(arr.begin(),arr.end());//this max is the
 //	minimum possible capacity of the tram (0 is allowed).
 }
+
