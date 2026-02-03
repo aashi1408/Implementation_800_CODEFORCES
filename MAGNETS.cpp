7133ec7ef0a5b606571883826d6e4d1b2a35d230
aashi1408 <aashiv2525@gmail.com>
2026-02-03 15:18:36 +0530

    (30)

---
+++ MAGNETS.cpp
@@ -0,0 +1,27 @@
+//MAGNETS
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int n;//number of magnets
+	cin>>n;
+	vector<string> magnets(n);
+	
+	for(int i=0 ; i<n ; i++)//take input of magnets
+	{
+		cin>>magnets[i];
+	}
+	int groups = 1;//atleast 1 magnet do exist
+	
+	for(int i=1 ; i<n ; i++)
+	{
+		if(magnets[i] != magnets[i-1])
+		{
+			groups++;
+		}
+	}
+	cout<<groups;
+}
