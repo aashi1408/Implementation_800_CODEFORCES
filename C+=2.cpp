e8c54a7456d7b8a9ef257b6fe4383da5d7ead133
aashi1408 <aashiv2525@gmail.com>
2026-02-13 20:47:06 +0530

    Add files via upload

---
+++ C+=2.cpp
@@ -0,0 +1,40 @@
+//C+=
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;//no. of tests
+	cin>>t;
+	
+	int a,b,n;
+	
+	while(t--)
+	{
+		cin>>a>>b>>n;
+		int mn = min(a,b);
+		int mx = max(a,b);
+		int count = 0;
+		bool flagie = false;
+		
+		while(mn <= n and mx <= n)//strictly greater than
+		{
+			if(mn <= n)
+			{
+				mn += mx;
+				count++;
+			}
+			if(mn > n or mx > n) 
+			{
+				break;
+			}
+			if(mx <= n)
+			{
+				mx += mn;
+				count++;
+			}
+		}
+		cout<<count<<endl;
+	}
+}
